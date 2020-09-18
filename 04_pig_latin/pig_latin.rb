50c9f1a78c210ee84ce0b3315201184d49cf7366
sinan cengiz <sinancengiz_84@hotmail.com>
2020-09-17 17:13:26 -0700

    question 04 answered

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,54 @@
 #write your code here
+def translate(word)
+    word_list = word.split(" ")
+    def trans_a_word(word)
+        puts word[0].downcase
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        if vowels.include?(word[0].downcase) == true
+            word += "ay"
+        elsif word[1].downcase == "q" and word[2].downcase == "u"
+            word += word[0]
+            word = word[1..word.length]
+            word += word[0]
+            word = word[1..word.length]
+            word += word[0]
+            word = word[1..word.length]
+            word += "ay"
+        elsif word[0].downcase == "q" and word[1].downcase == "u"
+            word += word[0]
+            word = word[1..word.length]
+            word += word[0]
+            word = word[1..word.length]
+            word += "ay"
+        elsif vowels.include?(word[0].downcase) != true and vowels.include?(word[1].downcase) != true and vowels.include?(word[2].downcase) != true
+            word += word[0]
+            word = word[1..word.length]
+            word += word[0]
+            word = word[1..word.length]
+            word += word[0]
+            word = word[1..word.length]
+            word += "ay"
+        elsif vowels.include?(word[0].downcase) != true and vowels.include?(word[1].downcase) != true
+            word += word[0]
+            word = word[1..word.length]
+            word += word[0]
+            word = word[1..word.length]
+            word += "ay"
+        else
+            word += word[0]
+            word = word[1..word.length]
+            word += "ay"
+        end
+        return word
+    end
+
+    result = ""
+    word_list.each do |w| 
+        result += trans_a_word(w)
+        result += " "
+    end
+    result = result[0..result.length-2]
+    return result
+end
+
+puts translate("cheery")
