6b3de17d2cb66f300d015cbde23c149782aa6f29
sinan cengiz <sinancengiz_84@hotmail.com>
2020-09-17 17:53:20 -0700

    question 05 answered

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
+
 class Book
 # write your code here
+    attr_accessor :title
+    def initilize(title = "")
+        @title = title 
+    end
+    def title
+        little_words = ["and", "or", "the", "over","an", "to", "the", "a", "but","above", "across", "against", "along", "among", "around", "at", "before", "behind", "below", "beneath", "beside", "between", "by", "down", "from", "in", "into", "near", "of", "off", "on", "to", "toward", "under", "upon", "with" , "within"]
+        result =  @title.split(" ").map {|word| 
+        if little_words.include? word
+            word
+        else
+        word.capitalize
+        end
+        }.join(" ")
+        result[0] = result[0].capitalize
+        return result
+    end
 end
+
